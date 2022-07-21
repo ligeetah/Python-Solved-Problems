587acf3e5554a86df3923a8d52fa11299a6a2381
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-07-22 00:38:27 +0500

    num

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-print(f"{}")
-
+from cgi import print_form
+from pprint import pformat
+from webbrowser import get
+print(get(here we get(not to get(here we go fast faster o)))) like ,my real it can be a bit odd here it can be goood like i told you before here is not bad at all atleast i can say google here is one off the best like i said before not at all go for it 
