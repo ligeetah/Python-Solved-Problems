efd375b4c51c2128e4ea1977fd1d9188527eaaf0
Abdul Mateen <abdulmateenkhanzwl@gmail.com>
2022-07-21 23:08:26 +0500

    23

---
+++ enumerate.py
@@ -0,0 +1,5 @@
+list1=[3,45.4,False,'mateen']
+# for item in list1:
+#     print(f'{item} ')
+for index,item in enumerate(list1):
+    print(f'{item} {index}')
---
+++ globalvar.py
@@ -0,0 +1,7 @@
+a=7
+def get():
+    global a
+    a=8
+    print(a)
+get()
+print(a)
